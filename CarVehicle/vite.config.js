8fd3f994906aaaf0799251002715b3543e98ffaf
barovinh <pv198357@gmail.com>
2026-05-27 19:59:22 +0700

    update

--- CarVehicle/vite.config.js
+++ CarVehicle/vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-            ],
-            refresh: true,
-            publicDirectory: "backend/public",
-            buildDirectory: "build",
-        }),
-        vue(),
-    ],
-
-    build: {
-        outDir: "backend/public/build",
-        emptyOutDir: true,
-    },
-});
+  plugins: [vue()],
+  build: {
+    outDir: 'backend/public/build',
+    emptyOutDir: true,
+  },
+})
